5635d2c891155ef8003c177d90add7f00dc1d311
IsaquePFerreira <92412320+IsaquePFerreira@users.noreply.github.com>
2024-06-03 21:57:22 -0300

    Create first steps

--- main.sh
+++ main.sh
@@ -3,7 +3,26 @@
 # Projects folder
 DEFAULT_PROJECT_FOLDER="$HOME/Projects"
 
+# languages list
+languages='1 - Bash
+2 - JavaScript'
+
 # DEFAULT_PROJECT_FOLDER exists?
 if [[ ! -d $DEFAULT_PROJECT_FOLDER ]]; then
 	mkdir $DEFAULT_PROJECT_FOLDER
 fi
+
+# Create new project
+# project name
+read -p 'Project name: ' project_name
+[[ -z $project_name ]] &&  echo 'Empty project name!' && exit 1
+# select a language
+echo 'Project language:'
+echo "$languages"
+read lang
+case $lang in
+	1) type_project='bash';;
+	2) type_project='javascript';;
+	*) echo 'Invalid or empty value' && exit 1;
+esac
+
